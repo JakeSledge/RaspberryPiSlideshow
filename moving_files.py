8ed9c88415fdf7c1f52c1d99901b696a01ccd3ce
jsledge19 <32575195+jsledge19@users.noreply.github.com>
2018-11-04 12:25:39 -0600

    Delete moving_files.py

--- moving_files.py
+++
@@ -1,8 +0,0 @@
-import shutil, os
-
-
-PATH_TO_PICS = '/Users/jakesledge/Desktop/Coding/Python/PhotoStreaming/Photos'
-PATH_TO_PLACE = '/Users/jakesledge/Desktop/Coding/Python/PhotoStreaming/'
-os.chdir(PATH_TO_PICS)
-print(os.listdir(PATH_TO_PLACE))
-shutil.copy(PATH_TO_PICS + '/dog.jpg', PATH_TO_PLACE)
